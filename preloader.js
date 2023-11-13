daacc130017e536594a07d69e3d9e5d6d1b355a2
fraevar <44650305+fraevar@users.noreply.github.com>
2023-11-13 19:17:06 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://jvsc99.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
+        path: 'https://fraevar.github.io/preloaderJSLottie/Animation - 1699894346443.json',
     });
 }
 
